e6f07c4ebcc716890b152338210a5aed4b7d5dca
Pedro Rodrigues <pedro.pfxr@gmail.com>
2022-12-10 17:31:26 +0000

    Adding code

---
+++ xmas_tree.ino
@@ -0,0 +1,152 @@
+#include<stdint.h>
+#include "pitches.h"
+
+
+// Pinout definition
+#define RCLK 5
+#define SRCLK 3
+#define DI 4
+#define BUZZ 9
+
+uint16_t blink = 0x5555;
+
+// Jingle Bells
+int melody[] = {
+  NOTE_E5, NOTE_E5, NOTE_E5,
+  NOTE_E5, NOTE_E5, NOTE_E5,
+  NOTE_E5, NOTE_G5, NOTE_C5, NOTE_D5,
+  NOTE_E5,
+  NOTE_F5, NOTE_F5, NOTE_F5, NOTE_F5,
+  NOTE_F5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5,
+  NOTE_E5, NOTE_D5, NOTE_D5, NOTE_E5,
+  NOTE_D5, NOTE_G5
+};
+
+int tempo[] = {
+  8, 8, 4,
+  8, 8, 4,
+  8, 8, 8, 8,
+  2,
+  8, 8, 8, 8,
+  8, 8, 8, 16, 16,
+  8, 8, 8, 8,
+  4, 4
+};
+
+// We wish you a merry Christmas
+
+int wish_melody[] = {
+  NOTE_B3, 
+  NOTE_F4, NOTE_F4, NOTE_G4, NOTE_F4, NOTE_E4,
+  NOTE_D4, NOTE_D4, NOTE_D4,
+  NOTE_G4, NOTE_G4, NOTE_A4, NOTE_G4, NOTE_F4,
+  NOTE_E4, NOTE_E4, NOTE_E4,
+  NOTE_A4, NOTE_A4, NOTE_B4, NOTE_A4, NOTE_G4,
+  NOTE_F4, NOTE_D4, NOTE_B3, NOTE_B3,
+  NOTE_D4, NOTE_G4, NOTE_E4,
+  NOTE_F4
+};
+
+int wish_tempo[] = {
+  4,
+  4, 8, 8, 8, 8,
+  4, 4, 4,
+  4, 8, 8, 8, 8,
+  4, 4, 4,
+  4, 8, 8, 8, 8,
+  4, 4, 8, 8,
+  4, 4, 4,
+  2
+};
+
+// Santa Claus is coming to town
+
+int santa_melody[] = {
+  NOTE_G4,
+  NOTE_E4, NOTE_F4, NOTE_G4, NOTE_G4, NOTE_G4,
+  NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, NOTE_C5,
+  NOTE_E4, NOTE_F4, NOTE_G4, NOTE_G4, NOTE_G4,
+  NOTE_A4, NOTE_G4, NOTE_F4, NOTE_F4,
+  NOTE_E4, NOTE_G4, NOTE_C4, NOTE_E4,
+  NOTE_D4, NOTE_F4, NOTE_B3,
+  NOTE_C4
+};
+
+int santa_tempo[] = {
+  8,
+  8, 8, 4, 4, 4,
+  8, 8, 4, 4, 4,
+  8, 8, 4, 4, 4,
+  8, 8, 4, 2,
+  4, 4, 4, 4,
+  4, 2, 4,
+  1
+};
+
+void setup() {
+  pinMode(RCLK, OUTPUT);
+  pinMode(SRCLK, OUTPUT);
+  pinMode(DI, OUTPUT);
+  pinMode(BUZZ, OUTPUT); // Buzzer
+
+  digitalWrite(RCLK, HIGH);
+}
+
+void buzz(int targetPin, long frequency, long length) {
+  long delayValue = 1000000 / frequency / 2; // calculate the delay value between transitions
+  //// 1 second's worth of microseconds, divided by the frequency, then split in half since
+  //// there are two phases to each cycle
+  long numCycles = frequency * length / 1000; // calculate the number of cycles for proper timing
+  //// multiply frequency, which is really cycles per second, by the number of seconds to
+  //// get the total number of cycles to produce
+  for (long i = 0; i < numCycles; i++) { // for the calculated length of time...
+    digitalWrite(targetPin, HIGH); // write the buzzer pin high to push out the diaphram
+    delayMicroseconds(delayValue); // wait for the calculated delay value
+    digitalWrite(targetPin, LOW); // write the buzzer pin low to pull back the diaphram
+    delayMicroseconds(delayValue); // wait again or the calculated delay value
+  }
+
+}
+
+void sing(int *melody, int *tempo, int size) {
+  for (int thisNote = 0; thisNote < size; thisNote++) {
+      // to calculate the note duration, take one second
+      // divided by the note type.
+      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+      int noteDuration = 1000 / tempo[thisNote];
+
+      write(blink);
+      blink ^= 0xffff;
+
+      buzz(BUZZ, melody[thisNote], noteDuration);
+      // to distinguish the notes, set a minimum time between them.
+      // the note's duration + 30% seems to work well:
+      int pauseBetweenNotes = noteDuration;// * 1.30;
+      delay(pauseBetweenNotes);
+      
+      // stop the tone playing:
+      buzz(BUZZ, 0, noteDuration);
+
+  }  
+}
+
+
+// Function to write to shift register
+void write(uint16_t value) {
+  digitalWrite(RCLK, LOW);
+  shiftOut(DI, SRCLK, MSBFIRST, value);
+  digitalWrite(RCLK, HIGH);
+}
+
+
+void loop() {
+  sing(santa_melody, santa_tempo, sizeof(santa_melody));
+  sing(santa_melody, santa_tempo, sizeof(santa_tempo));
+  delay(1000);
+  sing(wish_melody, wish_tempo, sizeof(wish_melody));
+  sing(wish_melody, wish_tempo, sizeof(wish_melody));
+  delay(1000);
+  sing(melody, tempo, sizeof(melody));
+  sing(melody, tempo, sizeof(melody));
+  delay(1000);
+}
